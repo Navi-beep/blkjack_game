694db6742483b0b535723bcbc69adedc0f4c12f0
Navi-Beep <estawars30@gmail.com>
2022-07-29 11:26:05 -0500

    added classes

---
+++ blackjack.py
@@ -0,0 +1,16 @@
+class Blackjack:
+    pass
+
+class cards:
+    pass
+
+class dealer:
+    pass
+
+class player:
+    pass
+
+
+#set up while loop, if player wants to hit, add to cards up to 21. 
+
+
