03ae9b59b257fb7fc5a9e5e3e1863b467318c4b5
Navi-Beep <estawars30@gmail.com>
2022-07-30 10:07:47 -0500

    added functions

--- blackjack.py
+++ blackjack.py
@@ -3,6 +3,25 @@ import random
 
 run = True
 
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def __repr__(self): 
+        return self.rank + ' of ' + self.suit
+
+    def __str__(self): 
+       return self.rank + ' of ' + self.suit
+
+    def get_card_value(self):
+        if isinstance(self.rank, int):
+            return self.rank
+        elif self.rank in {'J', 'K', 'Q'}:
+            return 10
+        else:
+            return 11
+
 class Blackjack:
     def __init__(self):
         self.deck = self.create_deck()
@@ -11,41 +30,23 @@ class Blackjack:
         suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         deck = [Card(suit,rank) for suit in suits for rank in ranks]
-        random.shuffle(deck)
+        #random.shuffle(deck)
         return deck
 
 
-    def shuffle(self):
+    def truffle_shuffle(self):
         random.shuffle(self.deck)  
 
     def deal(self):
         one_card = self.deck.pop()
         return one_card
 
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-    
-    def __str__(self): 
-        return self.rank + ' of ' + self.suit
-
-    
-    def get_card_value(self):
-        if isinstance(self.rank, int):
-            return self.rank
-        elif self.rank in {'J', 'K', 'q'}:
-            return 10
-        else:
-            return 11
-
-
 class Hand:
 
     def __init__(self):
         self.cards = []
         self.value = 0
-        self.aces = 0
+        
 
     def add_card(self, card):
         self.cards.append(card)
@@ -59,13 +60,15 @@ def hit(deck,hand):
     
 
 def hit_or_stand(deck, hand):
+    global run
+
     while True:
         response = input("What do you want to do? hit or stand? Press 'h' or 's': ")
 
         if response == 'h':
             hit(deck, hand)
 
-        elif response == 's':
+        elif response == 's': # not working, i need to fix this
             print("You are standing, it is the Dealer's turn to go!")
             run = False
 
@@ -78,19 +81,19 @@ def hit_or_stand(deck, hand):
 def show_first_cards(player, dealer):
     print("\nDealer hand: ")
     print("--first card hidden!--")
-    print(" ", dealer.cards[1])
+    print("", dealer.cards[1])
     print("\nPlayer hand: ",*player.cards)
 
 
 def show_rest_of_cards(player, dealer):
     print("\nDealer hand: ", *dealer.cards)
-    print("Dealer hand: ", *dealer.value)
-    print("\nPlayer's Hand: ", *player.cards)
-    print("\nPlayer hand: ",*player.value)
+    print("Dealer hand: ", dealer.value)
+    print("\nPlayer hand: ", *player.cards)
+    print("Player hand: ",player.value)
 
 
 def player_bust(player, dealer):
-    print("player busts! you lose!!!")
+    print("player busts! you lose!!! Nobody beats sub-zero!!")
 
 
 def dealer_bust(player, dealer):
@@ -108,14 +111,62 @@ def tie(player, dealer):
     print("It's a push! Nobody wins :(")
 
 
+while run:
+    
+    deck = Blackjack()
+    deck.truffle_shuffle()
+
+    player_cards = Hand()
+    player_cards.add_card(deck.deal())
+    player_cards.add_card(deck.deal())
+
+    dealer_cards = Hand()
+    dealer_cards.add_card(deck.deal())
+    dealer_cards.add_card(deck.deal())
 
 
+    show_first_cards(player_cards, dealer_cards)
 
+    while run:
 
+        hit_or_stand(deck, player_cards)
+        show_rest_of_cards(player_cards, dealer_cards)
 
+        if player_cards.value == 21:
+            player_wins(player_cards, dealer_cards)
 
+        elif player_cards.value > 21:
+            player_bust(player_cards, dealer_cards)
+            break
+            
 
+    if player_cards.value <= 21:
+        while dealer_cards.value < 17:
+            hit(deck, dealer_cards)
 
+            show_rest_of_cards(player_cards, dealer_cards)
 
+            if dealer_cards.value == 21:
+                dealer_wins(player_cards, dealer_cards)
 
+            elif dealer_cards.value > 21:
+                dealer_bust(player_cards, dealer_cards)
 
+            elif dealer_cards.value > player_cards.value: 
+                dealer_wins(player_cards, dealer_cards)
+
+            elif dealer_cards.value < player_cards.value: 
+                player_wins(player_cards, dealer_cards) 
+
+            elif player_cards.value > 21:
+                player_bust(player_cards, dealer_cards)
+
+    
+    start_new_session = input("Do you wanna play this game again??? Hit 'y' or 'n' ")
+    if start_new_session == 'y':
+        run = True
+        continue
+
+    else: 
+        print("Have a great day! No more gambling. Go home. Go hang out with your cat or something.\n")
+        
